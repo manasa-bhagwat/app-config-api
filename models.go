5145e0b07f09f5ac8759f835d13b5a63baccc63b
manasa <manasa.bhagwat.work@gmail.com>
2026-02-09 00:28:21 +0530

    Add Page and Widget models with JSON mapping

--- models.go
+++ models.go
@@ -1,1 +1,27 @@
 package main
+
+import (
+	"encoding/json"
+	"time"
+)
+
+// Page represents a mobile app screen configuration
+type Page struct {
+	ID        string    `json:"id"`
+	Name      string    `json:"name"`
+	Route     string    `json:"route"`
+	IsHome    bool      `json:"is_home"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+}
+
+// Widget represents a UI component placed on a page
+type Widget struct {
+	ID        string          `json:"id"`
+	PageID    string          `json:"page_id"`
+	Type      string          `json:"type"`
+	Position  int             `json:"position"`
+	Config    json.RawMessage `json:"config"` // flexible JSON storage
+	CreatedAt time.Time       `json:"created_at"`
+	UpdatedAt time.Time       `json:"updated_at"`
+}
